02e0d2c562f9fcd09cf6ec7162526718aab81ef7
Gert Peter Rue Brigsted <manisto@manisto.dk>
2018-01-28 20:55:16 +0100

    Working on adding angular cli.

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,12 @@
-import { AppModule } from './app.module';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import 'zone.js/dist/zone';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+import { AppModule } from "./app.module";
+
+if (false) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err));
