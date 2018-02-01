8507366f7567d7aed2c8db9aeaada99430df384c
Gert Peter Rue Brigsted <manisto@manisto.dk>
2018-02-01 21:24:50 +0100

    Server now running again.

--- dev.js
+++ dev.js
@@ -1,13 +1,1 @@
-require('./server/server');
-
-const rollup = require('rollup');
-const rollupConfig = require('./rollup.config');
-const watcher = rollup.watch(rollupConfig);
-
-watcher.on('event', (event) => {
-    if (event.code !== 'BUNDLE_END') {
-        return;
-    }
-
-    console.log(`Finished bundling ${event.input} (took ${event.duration} ms)`);
-});
+require("./server/server");
--- server/server.ts
+++ server/server.ts
@@ -1,16 +1,15 @@
-import * as path from 'path';
-import * as express from 'express';
-import { apiRouter } from './api';
+import * as path from "path";
+import * as express from "express";
+import { apiRouter } from "./api";
 
 let app = express();
 
-app.use('/app', express.static('app', { fallthrough: false }));
-app.use('/lib', express.static('node_modules', { fallthrough: false }));
-app.use('/api', apiRouter);
+app.use(express.static("client/dist"));
+app.use("/api", apiRouter);
 app.use(function(request, response, next) {
-    response.sendFile(path.join(process.cwd(), 'app/index.html'));
+  response.sendFile(path.join(process.cwd(), "client/dist/index.html"));
 });
 
 app.listen(1337, function() {
-    console.log('Now listening on http://localhost:1337...');
+  console.log("Now listening on http://localhost:1337...");
 });
